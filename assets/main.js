8e54566df2c9ef1d00a0680c4e7ccdc36d619c92
soniamshah <72034453+soniamshah@users.noreply.github.com>
2020-12-09 13:53:38 -0500

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,82 @@
+$(function() {
+  $(".first").hover(function() {
+    $("#first-image").toggleClass("show");
+  });
+});
+
+$(function() {
+  $(".second").hover(function() {
+    $("#second-image").toggleClass("show");
+  });
+});
+
+$(function() {
+  $(".fan-page").hover(function() {
+    $("#fan-page-image").toggleClass("show");
+  });
+});
+
+
+$(function() {
+  $(".app").hover(function() {
+    $("#app-image").toggleClass("show");
+  });
+});
+
+$(function() {
+  $(".still-life-1").hover(function() {
+    $("#still-life-1-image").toggleClass("show");
+  });
+});
+
+$(function() {
+  $(".still-life-2").hover(function() {
+    $("#still-life-2-image").toggleClass("show");
+  });
+});
+ 
+
+ $(function() {
+  $(".post-office").hover(function() {
+    $("#post-office-image").toggleClass("show");
+  });
+});
+
+  $(function() {
+  $(".love-story").hover(function() {
+    $("#love-story-image").toggleClass("show");
+  });
+});
+
+   $(function() {
+  $(".vote-type").hover(function() {
+    $("#vote-type-image").toggleClass("show");
+  });
+});
+
+   $(function() {
+  $(".netflix").hover(function() {
+    $("#netflix-image").toggleClass("show");
+  });
+});
+
+      $(function() {
+  $(".leaf").hover(function() {
+    $("#leaf-image").toggleClass("show");
+  });
+});
+
+        $(function() {
+  $(".lightswitch").hover(function() {
+    $("#lightswitch-image").toggleClass("show");
+  });
+});
+
+
+      $(function() {
+  $(".time").hover(function() {
+    $("#time-image").toggleClass("show");
+  });
+});
+
+
